5f663883a648439016ccb7183f6354e8c1d8473f
schaubes <schaubes@users.noreply.github.com>
2021-09-26 15:47:46 +0000

    deploy: 5cc54e176da661162b8674c6a4afd7924d489a57

---
+++ _nuxt/static/1632671265/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ffantasy-name-generator\u002F_nuxt\u002Fstatic\u002F1632671265",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ffantasy-name-generator\u002F",assetsPath:"\u002Ffantasy-name-generator\u002F_nuxt\u002F",cdnURL:a}}}}(null));
